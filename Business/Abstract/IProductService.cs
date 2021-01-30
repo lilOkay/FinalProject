720d05792f19cc4cc6c21bd767c90f0feee3bc75
lilOkay <gncokay@gmail.com>
2021-01-31 00:06:56 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {   //bütün ürünleri listeler
+        List<Product> GetAll();
+    }
+}
